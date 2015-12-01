tg/golang.tmbundle

commit cf4a25062bf453ca1a46a4ad7ef7ff01740f350b

Remove leading path separator (:) from GOPATH
If TM_GOPATH was not set, the resulting GOPATH was starting with
colon (i.e. ":$GOPATH"), which was causing troubles in go1.5

Cherrypick efa0658a654698577562f1ec2909b9d2f77bfad1 from tg

Support/gomate.rb
@@ -9,8 +9,7 @@ require "#{ENV['TM_SUPPORT_PATH']}/lib/tm/save_current_document"
 
 # TextMate's special GOPATH used in .tm_properties files prepended to the environment's GOPATH
 ENV['GOPATH'] = (ENV.has_key?('TM_GOPATH') ? ENV['TM_GOPATH'] : '') +
-                (ENV.has_key?('GOPATH') ? ':' + ENV['GOPATH'] : '')
-                
+                (ENV.has_key?('GOPATH') ? ':' + ENV['GOPATH'] : '').sub(/^:+/,'')
 module Go
   def Go::go(command, options={})
     # TextMate's special TM_GO or expect 'go' on PATH

commit 4372808d35a64b7934575c0572d0b7de3108cc14

REDO: Remove leading path separator (:) from GOPATH
If TM_GOPATH was not set, the resulting GOPATH was starting with
colon (i.e. ":$GOPATH"), which was causing troubles in go1.5

Reapply cherrypick of efa0658a654698577562f1ec2909b9d2f77bfad1 from tg.
Previously attempted in cf4a25062bf453ca1a46a4ad7ef7ff01740f350b,
accidentally overwritten in 26538901bcb6253660bf4fab52dbc336ed984e2c.
Issue identified by tg in https://github.com/syscrusher/golang.tmbundle/issues/17.

Support/gomate.rb
@@ -9,7 +9,7 @@ require "#{ENV['TM_SUPPORT_PATH']}/lib/tm/save_current_document"
 
 # TextMate's special GOPATH used in .tm_properties files prepended to the environment's GOPATH
 ENV['GOPATH'] = (ENV.has_key?('TM_GOPATH') ? ENV['TM_GOPATH'] : '') +
-                (ENV.has_key?('GOPATH') ? ':' + ENV['GOPATH'] : '')
+                (ENV.has_key?('GOPATH') ? ':' + ENV['GOPATH'] : '').sub(/^:+/,'')
 
 # Call tool to determine gopath
 if ENV.has_key?('TM_GO_DYNAMIC_GOPATH')